8b856b5f6bc4fd669fb568adcfea43eabf8d116a
kochd <koch@triple6.org>
2019-09-20 23:09:52 +0200

    better name for the screensaver

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 binary="eshd.exe"
 #wget -N -O mpv-dev-latest.7z "https://mpv.srsfckn.biz/mpv-dev-latest.7z" 
-gcc.exe -v -L /usr/include/ -o eshd.exe eshd.c -lmpv
+gcc.exe -v -L /usr/include/ -o $binary eshd.c -lmpv
 mkdir -p build
 cp $binary build/eshd.exe
-cp $binary build/eshd.scr
+cp $binary "build/Electric Sheep HD.scr"
 cp $binary /c/Windows/System32/eshd.scr
 zip build/eshd-scr.zip build/eshd.scr build/mpv-1.dll
